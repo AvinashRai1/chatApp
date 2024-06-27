3d56e3df84baf4b1f2a5e35dc1d39f11112b6855
Avinash Rai <100064737+AvinashRai1@users.noreply.github.com>
2024-06-27 15:00:06 +0530

    commit 2

--- backend/server.js
+++ backend/server.js
@@ -8,24 +8,22 @@ import messageRoutes from "./routes/message.route.js";
 import userRoutes from "./routes/user.route.js";    
 
 
-import connectToMongoDb from "./db/connectToMongoDB.js";
+import connectToMongoDb from "./db/connectToMongoDB.js"; 
 import cookieParser from "cookie-parser";
 
 
 dotenv.config(); 
 const __dirname = path.resolve(); 
-const PORT=process.env.PORT || 8000;      
+const PORT=process.env.PORT || 5000;       
 
 
 
 app.use(express.json());  
-app.use(cookieParser()); 
+app.use(cookieParser());   
 
 
  
-app.get("/",(req,res)=>{ 
-  res.send("hello india lets chat");     
-})
+
 app.use("/api/auth",authRoutes);  
 app.use("/api/messages",messageRoutes);
 app.use("/api/users",userRoutes);
